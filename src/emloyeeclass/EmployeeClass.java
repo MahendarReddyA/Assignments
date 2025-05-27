7bd2f01707908deba2ca69588f79eabbc3f239d1
MahendarReddyA <mahendar.a.reddy09@gmail.com>
2025-05-27 20:58:08 +0530

    Assignment is done

---
+++ src/emloyeeclass/EmployeeClass.java
@@ -0,0 +1,22 @@
+package emloyeeclass;
+
+public class EmployeeClass {
+
+
+		String Emp1Name ="Bharath";
+		int Emp1ID =1234;
+		
+		String Emp2Name ="ABC";
+		int Emp2Id =1235;
+		
+		String Emp3Name ="XYZ";
+		int Emp3Id =1236;
+
+public static void main(String[] args) {
+	EmployeeClass obj = new EmployeeClass();
+	System.out.println(obj.Emp1Name);
+	
+	
+}
+
+}
